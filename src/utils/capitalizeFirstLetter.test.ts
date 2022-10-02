0a4378a58e7779ab0a36007d96391317f9ee14cc
Dillon Hess <dnhess20@gmail.com>
2022-10-02 14:47:13 -0500

    added simple test

---
+++ src/utils/capitalizeFirstLetter.test.ts
@@ -0,0 +1,8 @@
+// Test to see if the function works as expected
+import {capitalizeFirstLetter} from './capitalizeFirstLetter';
+
+describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of a string', () => {
+        expect(capitalizeFirstLetter('hello')).toBe('Hello');
+    });
+});
