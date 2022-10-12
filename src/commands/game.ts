926d9fd2eb19725a4e3839e8042c0591fef8c7e9
Dillon Hess <dnhess20@gmail.com>
2022-10-12 10:40:41 -0500

    better pipeline

--- src/commands/game.ts
+++ src/commands/game.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { InteractionResponseFlags, InteractionResponseType } from 'discord-interactions';
