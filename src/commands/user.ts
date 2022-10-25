039b23e50ecd15998662ec3a94a3038ddc96624f
Seth <sethevanpeace@gmail.com>
2022-10-24 19:32:14 -0700

    Fixed complaints from the eslint department

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -1,9 +1,9 @@
 import { InteractionResponseType } from 'discord-interactions';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { params } from '@serverless/cloud';
-import DiscordInteraction from '../classes/DiscordInteraction';
+import type DiscordInteraction from '../classes/DiscordInteraction';
 import CommandOptionType from '../enums/ICommandOptionType';
-import { ICommand } from '../interfaces/ICommand';
+import type { ICommand } from '../interfaces/ICommand';
 
 export const command: ICommand = {
   name: 'user',
@@ -13,57 +13,61 @@ export const command: ICommand = {
       name: 'username',
       description: 'The username to look up',
       type: CommandOptionType.STRING,
-      required: true
+      required: true,
     },
   ],
 };
 
 interface IUserStatsResponse {
-    displayName: string;
-    bio: string;
-    icon: string;
-    description: string;
-    value: string;
-    stats: string[];
-    imageUrl: string;
+  displayName: string;
+  bio: string;
+  icon: string;
+  description: string;
+  value: string;
+  stats: any;
+  imageUrl: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const interact = async (interaction: DiscordInteraction, _interactionActionOverwrite?: any): Promise<any> => {
-  var startTime = performance.now();
+export const interact = async (
+  interaction: DiscordInteraction,
+  _interactionActionOverwrite?: any
+  ): Promise<any> => {
   const username = interaction.getOptionValue('username') as string;
 
   // Get leaderboard from API
-  const userStats = await fetch(`${params.BASE_API_URL}/users/${username}/stats`);
-  const userJson: IUserStatsResponse = await userStats.json();
-  // If leaderboard is empty, return error
-  if (!userStats) {
+  const userStats = await fetch(
+    `${params.BASE_API_URL}/users/${username}/stats`
+  );
+  try {
+    const userJson: IUserStatsResponse = await userStats.json();
     return {
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        content: 'User not found.',
+        embeds: [
+          {
+            title: userJson?.displayName,
+            description: userJson?.bio,
+            fields: userJson?.stats.map((statItem) => ({
+              name: `${statItem.icon} ${statItem.description}`.replace(
+                "undefined ",
+                ""),
+              value: statItem.value.toLocaleString(),
+              inline: true,
+            })),
+            thumbnail: {
+              url: userJson?.imageUrl,
+            },
+          },
+        ],
+      },
+    };
+  } catch(SyntaxError) {
+    return {
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: `User "${username}" not found.`,
       },
     };
   }
-  var endTime = performance.now()
-  userJson?.stats.map((statItem) => (console.log(statItem)));
-  return {
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      embeds: [
-        {
-          title: userJson?.displayName,
-          description: userJson?.bio,
-          fields: userJson?.stats.map((statItem) => ({
-            name: `${statItem["icon"]} ${statItem["description"]}`.replace("undefined ", ""),
-            value: statItem["value"].toLocaleString(),
-            inline: true,
-          })),
-          thumbnail: {
-            url: userJson?.imageUrl,
-          },
-        },
-      ],
-    },
-  };
 };
